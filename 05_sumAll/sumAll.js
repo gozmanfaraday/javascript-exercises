c7d192c995576e4fec80dab3028104d91e248051
Gozman <thegzmn@gmail.com>
2022-07-31 00:58:20 +0100

    Solve 05-sumAll/

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+  if (
+    (firstNum < 0 || lastNum < 0) ||
+    (typeof firstNum !== 'number' || typeof lastNum !== 'number')
+  ) {
+    return "ERROR";
+  } else {
+    if (firstNum < lastNum) {
+      let sum = firstNum;
+      for (let i = (firstNum + 1); i <= (lastNum); i++) {
+        sum = sum + i;
+      }
+  
+      return sum;
+    } else if (lastNum < firstNum) {
+      let sum = lastNum;
+      for (let i = (lastNum + 1); i <= (firstNum); i++) {
+        sum = sum + i;
+      }
+  
+      return sum;
+    } else {
+      sum = firstNum + lastNum;
+  
+      return sum;
+    }
+  }
 };
 
 // Do not edit below this line
