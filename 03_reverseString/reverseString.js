da814d7bf42bb710b9682c3009dd834f244affef
Gozman <thegzmn@gmail.com>
2022-07-30 20:09:43 +0100

    Solve 03_reverseString/

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let splitString = string.split("");
+  let reverseSplitString = splitString.reverse();
+  let reverseString = reverseSplitString.join("");
+  
+  return reverseString;
 };
 
 // Do not edit below this line
