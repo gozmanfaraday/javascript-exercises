b4363a21b4065506ff48f9d33dbd0da9653223c9
Gozman <thegzmn@gmail.com>
2022-07-30 20:08:40 +0100

    Solve 02_repeatString/

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let repeat = "";
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      repeat = repeat + string;
+    }
+  
+    return repeat;
+  }
 };
 
 // Do not edit below this line
