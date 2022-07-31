b5f42a83e9585fea1a7349c5a3129cc00693d0c3
Gozman <thegzmn@gmail.com>
2022-07-31 01:52:20 +0100

    Solve 07_tempConversion/

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let result = (fahrenheit - 32) * (5 / 9);
+  let celsius = (Math.round(result * 10)) / 10;
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let result = (celsius * (9 / 5)) + 32;
+  let fahrenheit = (Math.round(result * 10)) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
