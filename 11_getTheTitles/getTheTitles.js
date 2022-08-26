c3094e2c681b20d41cc402af103b3534c56b33fc
Gozman <thegzmn@gmail.com>
2022-08-27 00:35:55 +0100

    Update getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(items) {
+  const titles = items.map(item => {
+    return item.title;
+  })
 
+  return titles;
 };
 
 // Do not edit below this line
