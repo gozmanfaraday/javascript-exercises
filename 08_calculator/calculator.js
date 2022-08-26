f99dc67382c23741a864aba440df2ffb9e72994b
Gozman <thegzmn@gmail.com>
2022-08-26 19:56:27 +0100

    Update 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers = [], total = 0) {
+  numbers.forEach(number => {
+    total += number;
+  });
+  
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers = [], total = 1) {
+  numbers.forEach(number => {
+    total *= number;
+  });
+  
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let product = 1;
+
+	if (number === 0) {
+    return 1;
+  }
+
+  for (let i = 1; i <= number; i++) {
+    product *= i;
+  }
+
+  return product;
 };
 
 // Do not edit below this line
