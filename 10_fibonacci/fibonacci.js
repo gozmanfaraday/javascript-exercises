e7100aed6b50c4fdc94ab94e03816941fbfc70e9
Gozman <thegzmn@gmail.com>
2022-08-27 00:35:06 +0100

    Update 10_fibonacci/

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  number = Number(number);
+  let prev = 0;
+  let next = 1;
+  let sum = 1;
 
+  if (number < 1) {return "OOPS"};
+
+  for (let i = 1; i < number; i++) {
+    sum = prev + next;
+    prev = next;
+    next = sum;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
